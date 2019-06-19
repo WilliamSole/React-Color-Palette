d039ee7ec54d5f88d6ba0b6e0ad9cb567f913a8d
BOB\Will <williamsolebo@gmail.com>
2019-06-19 16:41:25 +0100

    Updates

---
+++ precache-manifest.d51c02658edbddae8edcf1e263d46329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a49d173aa972cee3c8f6af906a9f04",
+    "url": "/React-Color-Palette/index.html"
+  },
+  {
+    "revision": "b97933887f46a49e807b",
+    "url": "/React-Color-Palette/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "08300ece791411d0ec49",
+    "url": "/React-Color-Palette/static/css/main.7801206c.chunk.css"
+  },
+  {
+    "revision": "b97933887f46a49e807b",
+    "url": "/React-Color-Palette/static/js/2.18822c35.chunk.js"
+  },
+  {
+    "revision": "08300ece791411d0ec49",
+    "url": "/React-Color-Palette/static/js/main.b7323612.chunk.js"
+  },
+  {
+    "revision": "8ee53b4720c05ddfe88d",
+    "url": "/React-Color-Palette/static/js/runtime~main.e7110d86.js"
+  },
+  {
+    "revision": "66881ca9630bfb8bc2ff382a213542ee",
+    "url": "/React-Color-Palette/static/media/bg.66881ca9.svg"
+  }
+]);
